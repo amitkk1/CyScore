b4f5d00d08f4c8cde2e0d11f492e5956b11253c4
Amit <amitkastel3@gmail.com>
2022-05-01 13:37:28 +0300

    moved to new react api

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 //global styles imports
 import "./assets/fonts/roboto/stylesheet.css";
@@ -8,9 +8,10 @@ import "./assets/css/global.scss";
 import App from './App';
 
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  </React.StrictMode>
+)
